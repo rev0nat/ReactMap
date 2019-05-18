4904c87515fd502edad33ec0112711791763ff56
Anteo GUILLOT <anteo.guillot@gmail.com>
2019-05-18 21:44:44 +0200

    map showing after adding css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      <Map />
-      
+      <header>
+      <div id="map">
+      	<Map />
+      </div>
       </header>
     </div>
   );
